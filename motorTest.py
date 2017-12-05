11bc768e5437c7be29955bcc652931d36ff40c98
Simon Ivarsson <Simon.Ivarsson@sigma.se>
2017-12-05 14:24:03 +0100

    Add prints as well as stop funtionality to motor test

--- motorTest.py
+++ motorTest.py
@@ -10,12 +10,19 @@ while True:
     keyp = readkey()
     if keyp == 's' or ord(keyp) == 17:
         pz.reverse(speed)
+        print "Reverse..."
     elif keyp == 'w' or ord(keyp) == 16:
         pz.forward(speed)
+        print "Forward..."
     elif keyp == 'd' or ord(keyp) == 18:
         pz.spinRight(speed)
+        print "Right..."
     elif keyp == 'a' or ord(keyp) == 19:
         pz.spinLeft(speed)
+        print "Left..."
+    elif keyp == ' ':
+    	pz.stop()
+    	print "Stop..."
     elif ord(keyp) == 3:
         break
 
